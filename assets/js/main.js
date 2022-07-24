509739e1dbfc26b28d56d80295e1e40e4b8c72ec
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-07-25 02:16:44 +0400

    added nice select

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
-"use strict"
-let getMoneystate=document.getElementById("money");
-let getSellwithus=document.getElementById("sell-with-us"); 
-getMoneystate.addEventListener("click",(e)=>{    
-    e.preventDefault();
-    document.querySelector(".sell-with-us").classList.remove("d-block");
-    document.querySelector(".money-change").classList.toggle("d-block");
-})
+"use strict";
+let getMoneystate = document.getElementById("money");
+let getSellwithus = document.getElementById("sell-with-us");
+getMoneystate.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".sell-with-us").classList.remove("d-block");
+  document.querySelector(".money-change").classList.toggle("d-block");
+});
 
-getSellwithus.addEventListener("click",(e)=>{
-    e.preventDefault();
-    document.querySelector(".money-change").classList.remove("d-block");
-    document.querySelector(".sell-with-us").classList.toggle("d-block");
-
-})
+getSellwithus.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".money-change").classList.remove("d-block");
+  document.querySelector(".sell-with-us").classList.toggle("d-block");
+});
