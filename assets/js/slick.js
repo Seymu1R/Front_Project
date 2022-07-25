7c270db3ca7d37d00c3efe1a8446792d5e475ba8
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-07-26 02:25:05 +0400

    added footer and fix mega menus

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -5,12 +5,37 @@ $(document).ready(function() {
         infinite: false,
         speed: 300,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow:`
-        <button type="button" class="slider-prev">Previous</button>
-        `,
-        nextArrow:`
-        <button type="button" class="slider-next">Next</button>
-        `,
+        slidesToScroll: 1,        
+        
+        prevArrow: `<button type="button" class="slider-prev"><i class="fa-solid fa-arrow-left"></i></button>`,
+        nextArrow: `<button type="button" class="slider-next"><i class="fa-solid fa-arrow-right"></i></button>`,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 });
